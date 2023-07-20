a06bc209006ddb26bdcd56c376a03817cf0ee798
João Vitor <joaovitorcfdev@gmail.com>
2023-07-20 16:41:50 -0300

    create project

---
+++ src/test/java/com/joao/apprest/AppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.apprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
